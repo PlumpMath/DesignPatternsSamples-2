3c011d658043ce2af72f40ac7c5f353c8a68bd18
otaviolarrosa <otaviolarrosa@outlook.com>
2016-04-23 17:09:27 -0300

    Documentado código para explicação do Pattern

--- Patterns.AbstractFactory/Factories/FaturamentoPessoaFisica.cs
+++ Patterns.AbstractFactory/Factories/FaturamentoPessoaFisica.cs
@@ -3,13 +3,22 @@ using Patterns.AbstractFactory.Interfaces;
 
 namespace Patterns.AbstractFactory.Factories
 {
+    /// <summary>
+    /// Classe responsável por realizar o faturamento de pessoa física
+    /// </summary>
     public class FaturamentoPessoaFisica : IFaturamentoFactory
     {
+        /// <summary>
+        /// Método responsável por realizar o faturamento de pessoas físicas em lote
+        /// </summary>
         public IFaturamentoLote ProcessarFaturamentoLote()
         {
             return new Faturamento920();
         }
 
+        /// <summary>
+        /// Método responsável por realizar o faturamento de pessoas físicas individualmente
+        /// </summary>
         public IFaturamentoIndividual ProcessarFaturamentoIndividual()
         {
             return new Faturamento921();
--- Patterns.AbstractFactory/Factories/FaturamentoPessoaJuridica.cs
+++ Patterns.AbstractFactory/Factories/FaturamentoPessoaJuridica.cs
@@ -3,13 +3,22 @@ using Patterns.AbstractFactory.Interfaces;
 
 namespace Patterns.AbstractFactory.Factories
 {
+    /// <summary>
+    /// Classe responsável por realizar o faturamento de pessoa jurídica
+    /// </summary>
     public class FaturamentoPessoaJuridica : IFaturamentoFactory
     {
+        /// <summary>
+        /// Método responsável por realizar o faturamento de pessoas jurídicas em lote
+        /// </summary>
         public IFaturamentoLote ProcessarFaturamentoLote()
         {
             return new Faturamento640();
         }
 
+        /// <summary>
+        /// Método responsável por realizar o faturamento de pessoas jurídicas em individualmente
+        /// </summary>
         public IFaturamentoIndividual ProcessarFaturamentoIndividual()
         {
             return new Faturamento641();
